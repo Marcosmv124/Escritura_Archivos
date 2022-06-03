5921d75426b13dffee67bf4f34e112ccfd970ff1
Marcosmv124 <105815107+Marcosmv124@users.noreply.github.com>
2022-06-03 08:29:35 -0700

    Archivos-Tarea

---
+++ EscrituraArchivos/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO; 
+
+namespace EscrituraArchivos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StreamWriter sw = new StreamWriter("ejemplo.txt");
+            // Si el archivo no existe lo creara
+            // Si el archivo existe, escribira en el
+
+            string[] Lines =
+            {
+                "Esto es la primera Linea",
+                "Esto es la segunda Linea",
+                "Esto es la tercera Linea",
+                "Esto es la cuarta Linea"
+            };
+
+            foreach (string Line in Lines)
+            {
+                sw.WriteLine(Line);
+            }
+            sw.Close();
+
+            Console.WriteLine("Escribiendo........");
+            Console.ReadLine();
+        }
+    }
+}
